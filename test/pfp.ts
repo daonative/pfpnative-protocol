052b30864d0f370b0ef77aef9c64be8ad3fb9601
Laurent De Marez <laurent@daonative.xyz>
2022-04-23 15:07:20 +0200

    feat: add minting testcases

--- test/pfp.ts
+++ test/pfp.ts
@@ -1,8 +1,7 @@
 import { expect, use } from 'chai'
 import { solidity } from 'ethereum-waffle'
 import { jestSnapshotPlugin } from 'mocha-chai-jest-snapshot'
-import { ethers, waffle } from 'hardhat'
-import { parseEther } from '@ethersproject/units'
+import { ethers } from 'hardhat'
 
 use(solidity)
 use(jestSnapshotPlugin())
@@ -12,5 +11,33 @@ describe('PFP Contract', () => {
     const [owner] = await ethers.getSigners()
     const PFP = await ethers.getContractFactory('PFP')
     await PFP.deploy(owner.address, 'PFPNative', 'PFP')
+  })
+
+  it('should mint with a valid invite signature', async () => {
+    const [owner] = await ethers.getSigners()
+    const inviteCode = "invite-code"
+    const messageHash = ethers.utils.solidityKeccak256(['string'], [inviteCode]);
+    const signature = await owner.signMessage(ethers.utils.arrayify(messageHash))
+
+    const PFP = await ethers.getContractFactory('PFP')
+    const pfp = await PFP.deploy(owner.address, 'PFPNative', 'PFP')
+    await expect(pfp.safeMint(inviteCode, signature)).to.emit(pfp, 'Transfer')
+
+    const tokenSeed = await pfp.tokenSeed(0)
+    expect(tokenSeed.body).to.be.at.least(0)
+    expect(tokenSeed.body).to.be.at.most(3)
+    expect(tokenSeed.head).to.be.at.least(0)
+    expect(tokenSeed.head).to.be.at.most(3)
+  })
+
+  it('should not mint with an invalid invite signature', async () => {
+    const [owner, someoneElse] = await ethers.getSigners()
+    const inviteCode = "invite-code"
+    const messageHash = ethers.utils.solidityKeccak256(['string'], [inviteCode]);
+    const signature = await someoneElse.signMessage(ethers.utils.arrayify(messageHash))
+
+    const PFP = await ethers.getContractFactory('PFP')
+    const pfp = await PFP.deploy(owner.address, 'PFPNative', 'PFP')
+    await expect(pfp.safeMint(inviteCode, signature)).to.be.revertedWith('Invalid signature')
   })
 })
